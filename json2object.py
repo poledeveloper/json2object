99db6465c5e1c8b66b97ccf595cfe280424c144d
Federico Polesello <federico.polesello@metide.com>
2016-03-01 12:58:20 +0100

    add auto CamelCase, now files .h and .m are saved with the base class name, add traceback print on init method, remove class default separator '-', remove optional default prefix

--- json2object.py
+++ json2object.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import traceback
 
 class jsonObject:
 	
@@ -8,6 +9,8 @@ class jsonObject:
 	class_array = []
 	
 	file_name = ''
+
+	root_class = ''
 	
 	new_line 	 = "\n\n"
 	
@@ -23,25 +26,25 @@ class jsonObject:
 	
 	primitive_prefix = "@property (nonatomic, assign) "
 	 
-	number_prefix =  " NSNumber <Optional> "
+	number_prefix =  " NSNumber "
 	
 	number = " NSNumber "
 	
-	integer_prefix =  " NSInteger <Optional> "
+	integer_prefix =  " NSInteger "
 	
-	bool_prefix =  " BOOL <Optional> "
+	bool_prefix =  " BOOL "
 	
-	float_prefix =  " float <Optional> "
+	float_prefix =  " float "
 	
-	long_prefix =  " long <Optional> "
+	long_prefix =  " long "
 	
-	string_prefix = " NSString <Optional> "
+	string_prefix = " NSString "
 	
 	string = " NSString "
 	
 	pointer = " * "
 	
-	class_seperator = "_"
+	class_seperator = ""
 	
 	line_end = ";"
 	
@@ -56,12 +59,15 @@ class jsonObject:
 	def __init__(self,file_name,root_class):
 	
 		self.file_name = file_name
+		self.root_class = root_class
 		
 		try:
 			obj = json.loads(self.readFile(self.file_name) )
 			self.parseJson(obj,root_class,False)
 		except:
 			self.printError("invalid json string !")
+
+			traceback.print_exc()
         	
 		
 		
@@ -72,14 +78,14 @@ class jsonObject:
 	
 	def writeHeader2File(self):
 	
-		text_file = open(self.file_name + ".h", "w")
+		text_file = open(self.root_class + ".h", "w")
 		for  k in self.class_define_array:
 			text_file.write(k)
 		text_file.close()
 		
 	def writeImpl2File(self):
 	
-		text_file = open(self.file_name + ".m", "w")
+		text_file = open(self.root_class + ".m", "w")
 		for  k in self.class_array:
 			text_file.write(self.new_line + self.implementation + k+ self.new_line + self.end+self.new_line)
 		text_file.close()
@@ -88,8 +94,16 @@ class jsonObject:
         	file_handler = open(file_name)
         	file_text = file_handler.read()
 		return file_text
-		
-	
+
+	def firstLower(self, s):
+		if len(s) == 0:
+			return s
+		else:
+			return s[0].lower() + s[1:]
+
+	def convert(self, word):
+		res = "".join(x.capitalize() or '_' for x in word.split('_'))
+		return self.firstLower(res)
 
 	def parseJson (self, decoded_json_string, class_name,isprotocol ):
 		if isprotocol == True :
@@ -99,22 +113,22 @@ class jsonObject:
 		for  k in decoded_json_string.keys():
 			v = decoded_json_string[k]
 			if isinstance(v,float) or isinstance(v,int) or isinstance(v,long) or isinstance(v,bool):
-				str += self.new_line + self.object_prefix + self.number_prefix + self.pointer+ k + self.line_end
+				str += self.new_line + self.object_prefix + self.number_prefix + self.pointer+ self.convert(k) + self.line_end
 			elif isinstance(v,basestring) :
-				str += self.new_line + self.object_prefix +  self.string_prefix + self.pointer + k+ self.line_end
+				str += self.new_line + self.object_prefix +  self.string_prefix + self.pointer + self.convert(k) + self.line_end
 			elif isinstance(v,dict):
-				class_name = class_name + self.class_seperator + k
-				str += self.new_line + self.object_prefix + class_name + self.pointer + k + self.line_end
+				class_name = class_name + self.class_seperator + self.convert(k).capitalize()
+				str += self.new_line + self.object_prefix + class_name + self.pointer + self.convert(k) + self.line_end
 				self.parseJson(v,class_name,False)
 			elif isinstance(v,list) or isinstance(v,tuple) :
 				if len(v) == 0 :
-					self.printError( "key '" + k + "' can not be empty!");
+					self.printError( "key '" + self.convert(k) + "' can not be empty!");
 				value = v[0]
 				if isinstance(value,float) or isinstance(value,int) or isinstance(value,long) or isinstance(value,bool) or isinstance(value,basestring):
-					str += self.new_line + self.object_prefix + " NSMutableArray < Optional> * " + k + self.line_end
+					str += self.new_line + self.object_prefix + " NSMutableArray < Optional> * " + self.convert(k) + self.line_end
 				else:
-					class_name = class_name + self.class_seperator + k
-					str += self.new_line + self.object_prefix + " NSMutableArray <" + class_name + ",Optional> * " + k + self.line_end
+					class_name = class_name + self.class_seperator + self.convert(k).capitalize()
+					str += self.new_line + self.object_prefix + " NSMutableArray <" + class_name + ",Optional> * " + self.convert(k) + self.line_end
 					self.parseJson(v[0],class_name,True)
 		str += self.new_line+self.end+self.new_line
 		self.class_define_array.append(str)
